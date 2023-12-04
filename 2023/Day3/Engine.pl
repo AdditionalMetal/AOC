a440f646998b0963a7bf8bac50cb95d27d9fc2bc
Additonal Metal <67547350+AdditionalMetal@users.noreply.github.com>
2023-12-04 12:10:01 -0500

    Remap table, still need to figure out neighbors and add together...

--- 2023/Day3/Engine.pl
+++ 2023/Day3/Engine.pl
@@ -20,7 +20,6 @@ while(<>){
 }
 
 foreach my $i (1..$r){ # row
-  print "$i : ";
   foreach my $j (1..$c){ # column
     my $v = $map{"${i}:${j}"};
 
@@ -32,50 +31,33 @@ foreach my $i (1..$r){ # row
       my @store = ();
 
       foreach my $J ($j..$c){ # going towards eol...
-	print "Already stored... $i:$J \n" and last if ($remap{"${i}:${J}"}); # already stored...
+	  # print "Already stored... $i:$J \n" and
+	  last if ($remap{"${i}:${J}"}); # already stored...
 
 	my $digit = $map{"${i}:${J}"};
 	if ($digit =~ m/\d/){
 	  $num =  $num . $digit;
 	  push @store, "${i}:${J}";
-	  print "Preparing to store ${i}:${J} index...(now at @{store})\n";
+	  #print "Preparing to store ${i}:${J} index...(now at @{store})\n";
 	} else {
 	  $remap{"${i}:${J}"} = $map{"${i}:${J}"};
 
 	  # store in remap...
-	  print "********checking store: @store \n";
-	  map {"print using $_ index\n";
+	  #print "********checking store: @store \n";
+	  map {#"print using $_ index\n";
 	       $remap{$_} = $num;
 	     } @store;
-	  $num = '';
-	  @store = ();
 	  last;
 	}
       }
-
-      # # MEA CULPA - don't need to go in both directions,
-      # #             you just need to keep track of what changed
-      # foreach my $J (reverse (1..$j)){ # going towards start of line (sol?)...
-      # 	my $digit = $map{"${i}:${J}"};
-      # 	if ($digit =~ m/\d/){
-      # 	  $num =  $digit . $num;
-      # 	} else {
-      # 	  last;
-      # 	}
-      # }
-
-      # $remap{"${i}:${j}"} = 0 + $num;
-      # print " $num"
     } else {
       $remap{"${i}:${j}"} = $map{"${i}:${j}"};
-      print " $v";
+      #print "$v";
     }
   }
-  # Row...
-  print "\n";
 }
 
-print "\n";
+# print remap...
 foreach my $i (1..$r){ # row
   print "$i : ";
   foreach my $j (1..$c){ # column
